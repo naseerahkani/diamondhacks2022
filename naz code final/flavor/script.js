dfa06b24cb65a54da015b62cec6de3157eec44d1
naseerahkani <81028508+naseerahkani@users.noreply.github.com>
2022-03-27 10:46:47 -0400

    Add files via upload

---
+++ naz code final/flavor/script.js
@@ -0,0 +1,83 @@
+// console.log("runnign javacript file")
+// var feeling
+// var situation
+// dangerArray = 0
+// tierArray = 0
+
+// var danger1 = 0
+// var danger2 = 0
+// var danger3 = 0
+// var danger4 = 0
+// var danger5 = 0
+
+// window.onload = myFunction (); {
+// 	var situation = document.getElementById('flavor').value
+	
+// }
+// // window.onload= newFunction(); 
+
+
+//  function myFunction() {
+
+//  }
+// // for (var i = 0; i < dangerArray.length; i++){
+// // 	 situationCount = dangerArray[i];
+
+// // 	 if (situation == "toxic controlling") {
+// // 		 danger1++
+// // 		 document.getElementById('flavor').innerHTML="heyhey"
+// // 	 }
+
+// // 	else if (situation =="harassment"){
+// // 		danger2++
+// // 	}
+
+// // 	else if (situation == "physical abuse"){
+// // 		danger3++
+// // 	}
+
+// // 	else if (situation == "sexual assault") {
+// // 		danger4++
+// // 	}
+
+// // 	else if (situation == "danger") {
+// // 		danger5++
+// // 	}
+// // 	console.log(document.getElementById('flavor').value)
+// // }
+// //  }
+
+// //for(var i = 0; i < tierArray.length; i++){
+// //	tierCount = tierArray[i];
+// //
+// //	if (tier == "calm"){
+// //		danger1++	}
+
+// //	else if (tier == "uncomfortable"){
+// //		danger2++
+// //	}
+// //
+// //	else if (tier == "want to leave"){
+// //		danger3++
+// //	}
+// //	else if (tier =="send help"){
+// //		danger4++
+// //	}
+// //	else if (tier == "immediate danger"){
+// //		danger5++
+// //	}
+
+// //}
+
+
+// function newFunction() {
+// 	return document.getElementById('flavor').value
+// }
+
+
+var situation 
+
+function situationFunction(){
+	var situation = document.getElementById('flavor').value
+	console.log(situation)
+}
---
+++ naz code final/flavor/tierscript.js
@@ -0,0 +1,31 @@
+console.log ("yoyoyo")
+
+var feeling
+tierArray = 0
+
+window.onload = function () {
+var feeling = document.getElementById('tier').value
+}
+
+for(var i = 0; i < tierArray.length; i++){
+	tierCount = tierArray[i];
+
+	if (tier == "calm"){
+		danger1++
+	}
+
+	else if (tier == "uncomfortable"){
+		danger2++
+	}
+
+	else if (tier == "want to leave"){
+		danger3++
+	}
+	else if (tier =="send help"){
+		danger4++
+	}
+	else if (tier == "immediate danger"){
+		danger5++
+	}
+
+}
